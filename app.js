d4baea1d6d30530f92bdc28c4faf6d710055b602
theflyoccultist <y.delahaije@gmail.com>
2024-10-07 14:18:40 +0200

    babel transpile

--- app.js
+++
@@ -1,7 +0,0 @@
-import('./bot.js')
-    .then(() => {
-        console.log('Bot has started successfully')
-    })
-    .catch((err) => {
-        console.error('Failed to start the bot:', err);
-    })
